6157a10887d19861e15a7cffff82d86b6c7ee7b1
rudzinska.anya@gmail.com <rudzinska.anya@gmail.com>
2021-05-08 11:41:31 +0300

    edit2

--- shopwebsite/settings.py
+++ shopwebsite/settings.py
@@ -1,9 +1,11 @@
 import os
 from pathlib import Path
+import dj_database_url
+import django_heroku
+# from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -16,7 +18,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -26,9 +27,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shop',#'shop.apps.ShopConfig'
-    'cart',#'cart.apps.ShopConfig'
-    'orders',#'orders.apps.ShopConfig'
+    'shop',  # 'shop.apps.ShopConfig'
+    'cart',  # 'cart.apps.ShopConfig'
+    'orders',  # 'orders.apps.ShopConfig'
 ]
 
 MIDDLEWARE = [
@@ -39,6 +40,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'shopwebsite.urls'
@@ -62,7 +64,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'shopwebsite.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -72,7 +73,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -92,7 +92,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -106,12 +105,16 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL='/media/'
-MEDIA_ROOT=os.path.join(BASE_DIR,'media/')
 
-CART_SESSION_ID='cart'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
+LOGIN_URL = 'users:login'
+django_heroku.settings(locals())
+
+CART_SESSION_ID = 'cart'
